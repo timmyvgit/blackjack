4012042106a303b45c45c7c8429852ca634ef144
Tim Vuong <74928150+timmyvgit@users.noreply.github.com>
2023-04-06 23:11:47 -0500

    update

---
+++ blackjack.js
@@ -0,0 +1,6 @@
+var cards = [];
+var numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var suits = ["spades", "clubs", "diamonds", "hearts"];
+
+var randomNum = Math.floor((Math.random()*52)+1);
+console.log(randomNum);
